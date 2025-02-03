d7d47ef0b942a217b24d471cc034471ec43c7f2c
SwethaParthiban144 <2216121@saec.ac.in>
2025-02-03 11:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.listen('5000',()=>{console.log('Server is running on 5000')})
 
 
-mongoose.connect('mongodb+srv://2216121:Swetha146@mycluster.ivc0s.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster')
+mongoose.connect('mongodb+srv://2216121:Swetha144@mycluster.ivc0s.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster')
 .then(()=> console.log('database has been connected'))
 .catch((err)=>console.log(err))
 
